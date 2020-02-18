b311c8a890dadf72dd71ff3589be5924ec8a080b
Sony <maneendar0434@gmail.com>
2020-02-18 09:53:01 +0530

    Submit

--- Stack.py
+++ Stack.py
@@ -1,1 +1,15 @@
-print("tomorrow")
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def push(self,data):
+        self.items.insert(0,data)
+
+    def pop(self):
+        return self.items.pop()
+    def isEmpty(self):
+        return self.items==[]
+    def peek(self):
+        return self.items[len(self.items)-1]
+    def size(self):
+        return (self.items)
